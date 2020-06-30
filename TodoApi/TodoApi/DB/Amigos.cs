a749ea33dc594718a60d68c39052f80b9cefbbe9
pedrodiasparente <pdparente@gmail.com>
2020-06-30 18:43:12 +0100

    yes

---
+++ TodoApi/TodoApi/DB/Amigos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApi.DB
+{
+    public partial class Amigos
+    {
+        public int IdUser1 { get; set; }
+        public int IdUser2 { get; set; }
+
+        public virtual Utilizador IdUser1Navigation { get; set; }
+        public virtual Utilizador IdUser2Navigation { get; set; }
+    }
+}
